f43edb4ed8866fe4bf38a2f6112777c512005417
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-03-27 15:22:47 -0300

    Feat: Enhance IconButton component to support custom class names

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -1,11 +1,15 @@
+import { twMerge } from "tailwind-merge";
 import type { ComponentProps } from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function IconButton(props: ButtonProps) {
+export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        "p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900",
+        className
+      )}
       {...props}
     />
   );
